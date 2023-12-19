6203030aa35217e750f979231704ffdd348d4e16
Anfysa <104844855+Anfysa@users.noreply.github.com>
2023-12-19 12:06:42 +0300

    Update pz3.py

--- pz3.py
+++ pz3.py
@@ -1,11 +1,10 @@
 import cv2
 import numpy as np
 
-base_path = "image-processing-3/assets/"
 laser_centers = []
 
 def find_point_center(image_name):
-    image = cv2.imread(base_path + image_name, cv2.IMREAD_COLOR)
+    image = cv2.imread(image_name, cv2.IMREAD_COLOR)
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     _, binary_image = cv2.threshold(gray_image, 250, 255, cv2.THRESH_BINARY)
     contours, _ = cv2.findContours(binary_image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
